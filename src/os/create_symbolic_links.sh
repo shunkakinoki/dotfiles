8237973f503ce0db66090a307912e81e941a5677
Shun Kakinoki <shunkakinoki@gmail.com>
2019-10-27 13:27:24 +0900

    added src of dotfiles

--- src/os/create_symbolic_links.sh
+++ src/os/create_symbolic_links.sh
@@ -3,12 +3,8 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" \
     && . "utils.sh"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 create_symlinks() {
-
     declare -a FILES_TO_SYMLINK=(
-
         "shell/aliases/bash_aliases"
         "shell/autocomplete/$(get_os)/bash_autocomplete"
         "shell/bash_exports"
@@ -29,7 +25,6 @@ create_symlinks() {
 
         "vim/vim"
         "vim/vimrc"
-
     )
 
     local i=""
@@ -37,57 +32,41 @@ create_symlinks() {
     local targetFile=""
     local skipQuestions=false
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     skip_questions "$@" \
         && skipQuestions=true
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
     for i in "${FILES_TO_SYMLINK[@]}"; do
-
         sourceFile="$(cd .. && pwd)/$i"
         targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
         if [ ! -e "$targetFile" ] || $skipQuestions; then
-
             execute \
                 "ln -fs $sourceFile $targetFile" \
                 "$targetFile → $sourceFile"
 
         elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
             print_success "$targetFile → $sourceFile"
+
         else
-
             if ! $skipQuestions; then
-
                 ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
                 if answer_is_yes; then
-
                     rm -rf "$targetFile"
-
                     execute \
                         "ln -fs $sourceFile $targetFile" \
                         "$targetFile → $sourceFile"
-
                 else
                     print_error "$targetFile → $sourceFile"
                 fi
-
             fi
-
         fi
 
     done
-
 }
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 main() {
-    print_in_purple "\n • Create symbolic links\n\n"
+    print_in_purple "\n   Create symbolic links\n\n"
     create_symlinks "$@"
 }
 
 main "$@"
-Terms
