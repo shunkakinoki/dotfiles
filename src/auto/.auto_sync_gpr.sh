ac6f55a9e151158e3208406df4f64180c78dea01
Shun Kakinoki <shunkakinoki@gmail.com>
2020-05-12 08:04:25 +0900

    perf(auto-update): auto-sync-gpr

--- src/auto/.auto_sync_gpr.sh
+++ src/auto/.auto_sync_gpr.sh
@@ -2,8 +2,9 @@
 
 cd ~/dotfiles || exit
 git master-cleanse || true
-git aa
+git add -A
 HUSKY_SKIP_HOOKS=1 git cm "perf(auto-update): auto-sync-gpr"
 git checkout -b auto-sync-gpr
-git publish && hpn
+git publish
+hub pull-request --no-edit
 cd ~ || exit
