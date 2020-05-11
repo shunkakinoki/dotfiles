1c3dea64943840b65475f7f558485f391b851003
Shun Kakinoki <shunkakinoki@gmail.com>
2020-05-12 04:49:07 +0900

    perf(auto-update): auto-sync-gpr

--- src/os/main.sh
+++ src/os/main.sh
@@ -142,7 +142,6 @@ main() {
         ./create_symbolic_links.sh "$@"
         ./create_config_links.sh "$@"
         ./create_local_config_files.sh
-        ./create_tmuxinator_links.sh
     else
         printf "%s" "${BASH_SOURCE[0]}" | grep "setup.sh" &>/dev/null || download_dotfiles
         ./create_symbolic_links.sh "$@"
