52708db429376315cdbfe466174746ef733c00b9
Shun Kakinoki <shunkakinoki@gmail.com>
2025-02-09 16:21:22 +0900

    chore: update

--- nix/darwin/default.nix
+++ nix/darwin/default.nix
@@ -220,4 +220,26 @@
 
   # Used for backwards compatibility, please read the changelog before changing.
   system.stateVersion = 4;
+
+  # Fully declarative dock using the latest from Nix Store
+  local = {
+    dock.enable = true;
+    dock.entries = [
+      { path = "/Applications/Slack.app/"; }
+      { path = "/System/Applications/Messages.app/"; }
+      { path = "/System/Applications/Music.app/"; }
+      { path = "/Applications/Drafts.app/"; }
+      { path = "/System/Applications/Home.app/"; }
+      {
+        path = "${config.users.users.${user}.home}/.local/share/";
+        section = "others";
+        options = "--sort name --view grid --display folder";
+      }
+      {
+        path = "${config.users.users.${user}.home}/.local/share/downloads";
+        section = "others";
+        options = "--sort name --view grid --display stack";
+      }
+    ];
+  };
 }
