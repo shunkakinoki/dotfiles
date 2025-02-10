6ccc8fc14fff46eee67ca9067f8d87edd49bcb48
Shun Kakinoki <shunkakinoki@gmail.com>
2025-02-10 02:40:58 -0800

    chore: update

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -9,7 +9,7 @@ let
   fonts = import ./config/fonts.nix { inherit pkgs; };
   launchd = import ./config/launchd.nix { inherit pkgs; };
   # disable homebrew for runner
-  homebrew = if username != "runner" then (import ./config/homebrew.nix) else {};
+  homebrew = if username != "runner" then (import ./config/homebrew.nix) else { };
   networking = import ./config/networking.nix;
   nix = import ./config/nix.nix;
   security = import ./config/security.nix { inherit username; };
