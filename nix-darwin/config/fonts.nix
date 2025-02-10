c4c5734d4d90a2706ef4804c9fd3e8313ecdb0c9
Shun Kakinoki <shunkakinoki@gmail.com>
2025-02-10 13:50:22 +0900

    chore: update

--- nix-darwin/config/fonts.nix
+++ nix-darwin/config/fonts.nix
@@ -1,11 +1,6 @@
 { pkgs }:
 {
   fonts.packages = with pkgs; [
-    font-fira-mono
-    font-roboto-mono
-    font-source-code-pro
-    font-hack
-    font-jetbrains-mono
     nerd-fonts.fira-code
   ];
 }
