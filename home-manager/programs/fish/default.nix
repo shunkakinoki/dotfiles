11d0349db71699ca9616933dc4ab7fe95a86bf5f
Shun Kakinoki <shunkakinoki@gmail.com>
2025-02-17 02:11:30 +0900

    chore: update fish

--- home-manager/programs/fish/default.nix
+++ home-manager/programs/fish/default.nix
@@ -6,9 +6,25 @@
     interactiveShellInit = ''
       # disable fish greeting
       set fish_greeting
+      set fish_theme dracula
+      fish_add_path -p ~/.nix-profile/bin /nix/var/nix/profiles/default/bin
+      set -a fish_complete_path ~/.nix-profile/share/fish/completions/ ~/.nix-profile/share/fish/vendor_completions.d/
     '';
     shellAliases = {
       ls = "ls -G";
+    };
+    plugins = [
+      {
+        name = "autopair";
+        src = pkgs.fishPlugins.autopair-fish.src;
+      }
+      {
+        name = "hydro";
+        src = pkgs.fishPlugins.hydro.src;
+      }
+    ];
+    xdg.configFile."fish/themes/dracula.theme" = {
+      source = ./dracula.theme;
     };
   };
 }
