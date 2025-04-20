43f5d5bbed11879ffdcbbd38aee638409610195a
Shun Kakinoki <shunkakinoki@gmail.com>
2025-04-20 21:59:17 +0900

    refactor: update dotfilesUpdater import in default.nix to include pkgs for improved configuration handling

--- home-manager/services/default.nix
+++ home-manager/services/default.nix
@@ -2,7 +2,7 @@
 let
   docker = import ./docker { inherit pkgs; };
   ollama = import ./ollama { inherit pkgs; };
-  dotfilesUpdater = import ./dotfiles-updater;
+  dotfilesUpdater = import ./dotfiles-updater { inherit pkgs; };
 in
 [
   ollama
