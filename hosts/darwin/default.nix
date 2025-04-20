3760977c7e1cccb78909abb4c15b3243245f5a91
Shun Kakinoki <shunkakinoki@gmail.com>
2025-04-20 10:47:29 +0000

    refactor: update configuration structure in default.nix to use an empty attribute set for clarity

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -31,7 +31,7 @@ nix-darwin.lib.darwinSystem {
         inherit inputs username;
         lib = inputs.nixpkgs.lib;
         pkgs = inputs.nixpkgs.legacyPackages.${system};
-        config = {};
+        config = { };
       };
     }
   ];
