8e746b510b743356caf4909c3f8ce7a2bcfd6f9f
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2025-05-23 19:26:03 +0900

    Add-packages (#62)
    * chore(nix): add jq and uv to package list for improved functionality
    
    * chore(nix): clean up default.nix by removing commented-out code

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -7,24 +7,15 @@
   ...
 }:
 let
-  # nvfetcher
-  # sources = pkgs.callPackage ../_sources/generated.nix { };
-
   hmConfig = import ../config;
-
   overlay = import ./overlay;
   packages = import ./packages { inherit pkgs; };
-
   misc = import ./misc;
-
   modules = import ./modules;
-
   programs = import ./programs {
     inherit lib pkgs;
     sources = { };
   };
-
-  # services
   services = import ./services {
     inherit pkgs;
   };
@@ -34,11 +25,8 @@ in
   imports = hmConfig ++ misc ++ modules ++ programs ++ services;
 
   home.username = username;
-
   home.homeDirectory = lib.mkIf pkgs.stdenv.isLinux "/home/${username}";
-
   home.packages = packages;
-
   home.stateVersion = "24.11";
 
   accounts.email.accounts = {
