fae08d0a17439ee35055f6d00bc2bcc80bedcccc
Shun Kakinoki <shunkakinoki@gmail.com>
2025-05-29 11:28:54 +0900

    fix(dock): create dock configuration and import it into the system

---
+++ nix-darwin/config/dock.nix
@@ -0,0 +1,35 @@
+{
+  system.defaults.dock = {
+    autohide = true;
+    autohide-delay = 0.0;
+    orientation = "bottom";
+    tilesize = 42;
+    showhidden = true;
+    show-recents = true;
+    show-process-indicators = true;
+    expose-animation-duration = 0.1;
+    expose-group-apps = true;
+    launchanim = false;
+    mineffect = "scale";
+    mru-spaces = false;
+    persistent-apps = [
+      "/System/Applications/Reminders.app"
+      "/System/Applications/Notes.app"
+      "/Applications/ChatGPT.app"
+      "/Applications/Google Chrome.app"
+      "/Applications/Docker.app"
+      "/Applications/Ghostty.app"
+      "/Applications/Linear.app"
+      "/Applications/Notion.app"
+      "/Applications/Cursor.app"
+      "/Applications/Visual Studio Code.app"
+      "/System/Applications/Messages.app"
+      "/Applications/Beeper Desktop.app"
+      "/Applications/Slack.app"
+      "/Applications/Discord.app"
+      "/System/Applications/Music.app"
+      "/System/Applications/Podcasts.app"
+      "/System/Applications/System Settings.app"
+    ];
+  };
+}
--- nix-darwin/config/system.nix
+++ nix-darwin/config/system.nix
@@ -44,35 +44,6 @@
         ShowPathbar = true;
         ShowStatusBar = true;
       };
-      dock = {
-        autohide = true;
-        autohide-delay = 0.0;
-        orientation = "bottom";
-        tilesize = 42;
-        showhidden = true;
-        show-recents = true;
-        show-process-indicators = true;
-        expose-animation-duration = 0.1;
-        expose-group-apps = true;
-        launchanim = false;
-        mineffect = "scale";
-        mru-spaces = false;
-        persistent-apps = [
-          "/System/Applications/Reminders.app"
-          "/System/Applications/Notes.app"
-          "/Applications/Google Chrome.app"
-          "/Applications/Docker.app"
-          "/Applications/Ghostty.app"
-          "/Applications/Cursor.app"
-          "/Applications/Visual Studio Code.app"
-          "/System/Applications/Messages.app"
-          "/Applications/Slack.app"
-          "/Applications/Discord.app"
-          "/System/Applications/Music.app"
-          "/System/Applications/Podcasts.app"
-          "/System/Applications/System Settings.app"
-        ];
-      };
       trackpad = {
         Clicking = true;
         Dragging = true;
