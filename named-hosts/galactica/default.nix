e70bb980eb193c37b80d750cc9289a0552a67f3c
Shun Kakinoki <shunkakinoki@gmail.com>
2025-06-29 09:36:29 +0900

    chore: commit

--- named-hosts/galactica/default.nix
+++ named-hosts/galactica/default.nix
@@ -19,8 +19,50 @@ inputs.nix-darwin.lib.darwinSystem {
           matchBlocks = {
             "*" = {
               identityFile = inputs.nixpkgs.lib.mkForce "/run/agenix/keys/id_ed25519.age";
+              extraOptions = {
+                AddKeysToAgent = "yes";
+                UseKeychain = "yes";
+              };
             };
           };
+        };
+        programs.git = {
+          signing = {
+            signByDefault = true;
+            key = "~/.ssh/id_ed25519";
+          };
+          extraConfig = {
+            commit.gpgSign = true;
+            gpg.format = "ssh";
+            gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+            user.signingKey = "~/.ssh/id_ed25519";
+          };
+        };
+        home.file.".ssh/allowed_signers" = {
+          text = "shunkakinoki@gmail.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEKze2jlpV7SyTKA2ezqbumpCiDn+5Sj4z5SxrqfzesX shunkakinoki@gmail.com";
+        };
+        
+        # Environment variables for SSH agent
+        home.sessionVariables = {
+          GPG_TTY = "$(tty)";
+        };
+        
+        # Configure pinentry for macOS
+        programs.gpg = {
+          enable = true;
+          settings = {
+            pinentry-program = "${pkgs.pinentry_mac}/bin/pinentry-mac";
+          };
+        };
+        
+        # SSH configuration for macOS keychain integration
+        home.file.".ssh/config" = {
+          text = ''
+            Host *
+              AddKeysToAgent yes
+              UseKeychain yes
+              IdentitiesOnly yes
+          '';
         };
       };
     }
