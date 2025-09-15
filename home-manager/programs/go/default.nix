f3d2db88f72b3ab34e54cbc8991ae790d89bd1bb
Shun Kakinoki <shunkakinoki@gmail.com>
2025-09-15 17:59:15 +0900

    refactor(go): simplify Go configuration in default.nix
    Removed commented-out environment variables and shell initialization for Go, streamlining the configuration. This change enhances clarity and prepares for future adjustments in the Go setup within home-manager.

--- home-manager/programs/go/default.nix
+++ home-manager/programs/go/default.nix
@@ -3,14 +3,4 @@
   home.packages = with pkgs; [
     go
   ];
-
-  programs.fish.interactiveShellInit = ''
-    fish_add_path -p ~/go/bin
-  '';
-  programs.go = {
-    env = {
-      GOBIN = "$HOME/go/bin";
-      GOPATH = "$HOME/go";
-    };
-  };
 }
