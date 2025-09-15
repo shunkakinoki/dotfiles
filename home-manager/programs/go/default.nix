982152b077538005fe3be740f0924630691f728c
Shun Kakinoki <shunkakinoki@gmail.com>
2025-09-15 17:46:03 +0900

    refactor: comment out nixpkgs input in home-manager configuration
    Temporarily commented out the nixpkgs input reference in the home-manager section of flake.nix to enhance clarity and prepare for future adjustments. This change maintains the existing structure while simplifying the configuration.

--- home-manager/programs/go/default.nix
+++ home-manager/programs/go/default.nix
@@ -3,10 +3,10 @@
   programs.fish.interactiveShellInit = ''
     fish_add_path -p ~/go/bin
   '';
-  # programs.go = {
-  #   enable = true;
-  #   package = pkgs.go;
-  #   goPath = "go";
-  #   goBin = "go/bin";
-  # };
+  programs.go = {
+    enable = true;
+    # package = pkgs.go;
+    goPath = "go";
+    goBin = "go/bin";
+  };
 }
