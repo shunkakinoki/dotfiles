9cc4460c0ec027c096a83d043570b9a524333403
Shun Kakinoki <shunkakinoki@gmail.com>
2025-09-19 23:41:43 +0900

    refactor: use symlink approach for Crush config
    - Change from inline text to external file reference
    - Follow codex module pattern with mkOutOfStoreSymlink
    - Create separate crush.json configuration file
    - Maintain same attribution settings (co_authored_by: false)
    - Improve consistency with existing module conventions

--- home-manager/modules/crush/default.nix
+++ home-manager/modules/crush/default.nix
@@ -1,24 +1,6 @@
+{ config, ... }:
 {
-  config,
-  pkgs,
-  lib,
-  ...
-}:
-{
-  home.file.".config/crush/crush.json".text = ''
-    {
-      "$schema": "https://charm.land/crush.json",
-      "options": {
-        "attribution": {
-          "co_authored_by": false,
-          "generated_with": true
-        }
-      },
-      "lsp": {
-        "nix": {
-          "command": "nil"
-        }
-      }
-    }
-  '';
+  home.file.".config/crush/crush.json" = {
+    source = config.lib.file.mkOutOfStoreSymlink ./crush.json;
+  };
 }
