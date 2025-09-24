4fd0dd3e400c9d10415bf4575bd922815d780a17
Shun Kakinoki <shunkakinoki@gmail.com>
2025-09-25 05:20:06 +0900

    feat: integrate ollama service for codex workflows

--- home-manager/services/default.nix
+++ home-manager/services/default.nix
@@ -2,8 +2,10 @@
 let
   codeSyncer = import ./code-syncer { inherit pkgs; };
   dotfilesUpdater = import ./dotfiles-updater { inherit pkgs; };
+  ollama = import ./ollama { inherit pkgs; };
 in
 [
   codeSyncer
   dotfilesUpdater
+  ollama
 ]
--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -9,7 +9,6 @@ let
   dock = import ./config/dock.nix;
   fonts = import ./config/fonts.nix { inherit pkgs; };
   homebrew = import ./config/homebrew.nix { inherit isRunner; };
-  launchd = import ./config/launchd.nix;
   networking = import ./config/networking.nix;
   nix = import ./config/nix.nix;
   security = import ./config/security.nix { inherit username; };
@@ -21,7 +20,6 @@ in
     dock
     fonts
     homebrew
-    launchd
     networking
     nix
     security
