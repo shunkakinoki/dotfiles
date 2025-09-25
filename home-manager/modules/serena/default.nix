59c2030f5ffbbb9f843af0ba54a5862341745f78
Shun Kakinoki <shunkakinoki@gmail.com>
2025-09-26 05:21:51 +0900

    feat: replace serena_config.template.yml with serena_config.yml for configuration management

--- home-manager/modules/serena/default.nix
+++ home-manager/modules/serena/default.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  home.file.".serena/serena_config.template.yml" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./serena_config.template.yml;
+  home.file.".serena/serena_config.yml" = {
+    source = config.lib.file.mkOutOfStoreSymlink ./serena_config.yml;
   };
 }
