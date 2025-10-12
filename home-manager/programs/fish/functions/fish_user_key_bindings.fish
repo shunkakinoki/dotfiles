a98a240ed1b90b6e04b34e66a7115318f4862f66
Shun Kakinoki <shunkakinoki@gmail.com>
2025-10-12 19:05:07 +0900

    feat: add custom key bindings for exiting vim insert mode in fish shell

---
+++ home-manager/programs/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,8 @@
+# Custom key bindings for Fish shell
+# Note: This filename must NOT have an underscore prefix (_) because Fish's
+# autoloading mechanism requires the filename to match the function name exactly.
+# Fish automatically calls this function after fish_vi_key_bindings is set.
+function fish_user_key_bindings
+  # Exit vim insert mode with jj (like in Vim)
+  bind -M insert -m default jj backward-char force-repaint
+end
