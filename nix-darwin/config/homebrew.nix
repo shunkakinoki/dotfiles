8eb826b7a6c3299bfb590e16315308abae2b3641
Shun Kakinoki <shunkakinoki@gmail.com>
2025-10-19 17:20:44 +0900

    feat(homebrew): add "sst/tap" to taps and "sst/tap/opencode" to brews; remove "opencode" from casks

--- nix-darwin/config/homebrew.nix
+++ nix-darwin/config/homebrew.nix
@@ -12,6 +12,7 @@
       "homebrew/cask"
       "kurtosis-tech/tap"
       "oven-sh/bun"
+      "sst/tap"
     ];
     brews = [
       "bun"
@@ -30,6 +31,7 @@
       "postgresql"
       "protobuf"
       "sheldon"
+      "sst/tap/opencode"
       "temporal"
     ];
     casks = [
@@ -61,7 +63,6 @@
       "lm-studio"
       "notion"
       "ollama-app"
-      "opencode"
       "raycast"
       "rescuetime"
       "screen-studio"
