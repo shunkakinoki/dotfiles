2e30626b13ee619f657da9d6db986e2cbbdbf8c9
Shun Kakinoki <shunkakinoki@gmail.com>
2025-11-08 10:34:09 +0900

    refactor(git): standardize formatting in default.nix configuration
    - Reformatted the Git configuration in default.nix for improved readability and consistency.
    - No functional changes were made to the configuration settings.

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -21,61 +21,61 @@
           unshallow = "fetch --prune --tags --unshallow";
         };
         core = {
-        editor = "nvim";
-        compression = -1;
-        autocrlf = "input";
-        whitespace = "trailing-space,space-before-tab";
-        precomposeunicode = true;
-        ignorecase = false;
-      };
-      color = {
-        diff = "auto";
-        status = "auto";
-        branch = "auto";
-        ui = true;
-      };
-      advice = {
-        addEmptyPathspec = false;
-      };
-      apply = {
-        whitespace = "nowarn";
-      };
-      help = {
-        autocorrect = 1;
-      };
-      grep = {
-        extendRegexp = true;
-        lineNumber = true;
-      };
-      push = {
-        autoSetupRemote = true;
-        default = "simple";
-      };
-      submodule = {
-        fetchJobs = 4;
-      };
-      log = {
-        showSignature = false;
-      };
-      format = {
-        signOff = true;
-      };
-      rerere = {
-        enabled = true;
-      };
-      pull = {
-        ff = "only";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      delta = {
-        navigate = true;
-        dark = true;
-      };
-      merge = {
-        conflictStyle = "zdiff3";
-      };
+          editor = "nvim";
+          compression = -1;
+          autocrlf = "input";
+          whitespace = "trailing-space,space-before-tab";
+          precomposeunicode = true;
+          ignorecase = false;
+        };
+        color = {
+          diff = "auto";
+          status = "auto";
+          branch = "auto";
+          ui = true;
+        };
+        advice = {
+          addEmptyPathspec = false;
+        };
+        apply = {
+          whitespace = "nowarn";
+        };
+        help = {
+          autocorrect = 1;
+        };
+        grep = {
+          extendRegexp = true;
+          lineNumber = true;
+        };
+        push = {
+          autoSetupRemote = true;
+          default = "simple";
+        };
+        submodule = {
+          fetchJobs = 4;
+        };
+        log = {
+          showSignature = false;
+        };
+        format = {
+          signOff = true;
+        };
+        rerere = {
+          enabled = true;
+        };
+        pull = {
+          ff = "only";
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        delta = {
+          navigate = true;
+          dark = true;
+        };
+        merge = {
+          conflictStyle = "zdiff3";
+        };
       };
       ignores = lib.splitString "\n" (builtins.readFile ./.gitignore.global);
     };
