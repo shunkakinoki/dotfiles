3d80e34988bb05562fe4906c34055ff886114b5c
Shun Kakinoki <shunkakinoki@gmail.com>
2025-11-21 03:03:13 -0300

    chore: add automatic brew upgrader service
    - Add brew-upgrader service module for macOS launchd
    - Runs brew upgrade every 3 hours with logging
    - Integrates into home-manager services
    - Conditional on Darwin (macOS) platform

---
+++ home-manager/services/brew-upgrader/default.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+{
+  launchd.agents.brew-upgrader = pkgs.lib.mkIf pkgs.stdenv.isDarwin {
+    enable = true;
+    config = {
+      ProgramArguments = [
+        "${pkgs.bash}/bin/bash"
+        "${./upgrade.sh}"
+      ];
+      RunAtLoad = true;
+      KeepAlive = true;
+      StartInterval = 10800;
+      StandardOutPath = "/tmp/brew-upgrader.log";
+      StandardErrorPath = "/tmp/brew-upgrader.error.log";
+    };
+  };
+}
