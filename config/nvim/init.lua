dbe83f4842c4e8ee1fd6e25037026651efb1eb23
Shun Kakinoki <shunkakinoki@gmail.com>
2025-11-22 09:17:22 -0600

    refactor(nvim): switch color scheme from gruvbox to dracula
    - Updated the Neovim configuration to replace the gruvbox color scheme with dracula.
    - Adjusted related settings in the lualine plugin to reflect the new theme.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -71,7 +71,7 @@ inoreabbrev sao são
 vim.pack.add({
 	-- UI
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
+	{ src = "https://github.com/Mofiqul/dracula.nvim" },
 	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
 	{ src = "https://github.com/stevearc/dressing.nvim" },
 	{ src = "https://github.com/rcarriga/nvim-notify" },
@@ -183,22 +183,7 @@ end
 ---
 
 local bg0 = "#1b1b1b"
-require("gruvbox").setup({
-	contrast = "hard",
-	overrides = {
-		GruvboxBg0 = { fg = bg0 },
-		SignColumn = { bg = bg0 },
-		GruvboxRedSign = { bg = bg0 },
-		GruvboxYellowSign = { bg = bg0 },
-		GruvboxGreenSign = { bg = bg0 },
-		GruvboxAquaSign = { bg = bg0 },
-		GruvboxOrangeSign = { bg = bg0 },
-		GruvboxPurpleSign = { bg = bg0 },
-		GruvboxBlueSign = { bg = bg0 },
-		Normal = { bg = bg0 },
-	},
-})
-vim.cmd("colorscheme gruvbox")
+vim.cmd("colorscheme dracula")
 -- https://github.com/ellisonleao/gruvbox.nvim/commit/c74e6e523c4827b67af2cb02b636e22dc35fdf70
 -- https://github.com/nvim-lualine/lualine.nvim/issues/1312
 vim.api.nvim_set_hl(0, "StatusLine", { reverse = false })
@@ -217,7 +202,7 @@ local section_b = { "branch", "diff", { "diagnostics", sources = { "nvim_workspa
 local section_c = { "%=", { "filename", file_status = false, path = 1 } }
 require("lualine").setup({
 	options = {
-		theme = "gruvbox",
+		theme = "dracula",
 		component_separators = "",
 		section_separators = "",
 	},
