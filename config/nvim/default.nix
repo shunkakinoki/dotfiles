c4e2489fdd6da04f4c9e3a357bb64261fad398e8
Shun Kakinoki <shunkakinoki@gmail.com>
2025-11-22 18:47:21 -0600

    chore: update

--- config/nvim/default.nix
+++ config/nvim/default.nix
@@ -1,9 +1,9 @@
 { config, ... }:
-let
-  dotfilesDir = "${config.home.homeDirectory}/dotfiles";
-in
 {
-  xdg.configFile."nvim" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/config/nvim";
+  home.file.".config/nvim/init.lua" = {
+    source = config.lib.file.mkOutOfStoreSymlink ./init.lua;
+  };
+  home.file.".config/nvim/nvim-pack-lock.json" = {
+    source = config.lib.file.mkOutOfStoreSymlink ./nvim-pack-lock.json;
   };
 }
