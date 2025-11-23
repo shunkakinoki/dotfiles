64bcfbf0b52a6185b5b20bdf572867828878ab6c
Shun Kakinoki <shunkakinoki@gmail.com>
2025-11-23 20:23:48 +0900

    chore: add force option to symlinks for Neovim configuration files

--- config/nvim/default.nix
+++ config/nvim/default.nix
@@ -2,8 +2,10 @@
 {
   home.file.".config/nvim/init.lua" = {
     source = config.lib.file.mkOutOfStoreSymlink ./init.lua;
+    force = true;
   };
   home.file.".config/nvim/nvim-pack-lock.json" = {
     source = config.lib.file.mkOutOfStoreSymlink ./nvim-pack-lock.json;
+    force = true;
   };
 }
