1f5e93ed95215cd70929d537188804c886fe1f17
Shun Kakinoki <shunkakinoki@gmail.com>
2025-11-24 15:24:15 +0900

    fix: change nvim-pack-lock.json from symlink to copy to allow Neovim write access

--- config/nvim/default.nix
+++ config/nvim/default.nix
@@ -4,8 +4,10 @@
     source = config.lib.file.mkOutOfStoreSymlink ./init.lua;
     force = true;
   };
+  # Copy nvim-pack-lock.json instead of symlinking so Neovim can write to it
+  # Use make neovim-sync to sync changes back to the repo
   home.file.".config/nvim/nvim-pack-lock.json" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./nvim-pack-lock.json;
+    source = ./nvim-pack-lock.json;
     force = true;
   };
 }
