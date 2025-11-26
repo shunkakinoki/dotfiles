4009ba2511d1f90a98a08d2e8dc91d599b7f2503
Shun Kakinoki <shunkakinoki@gmail.com>
2025-11-26 16:28:04 +0900

    feat: remove unused devenv package from Nix configuration

--- home-manager/packages/default.nix
+++ home-manager/packages/default.nix
@@ -8,7 +8,6 @@ in
 with pkgs;
 [
   inputs.agenix.packages.${stdenv.hostPlatform.system}.default
-  inputs.devenv.packages.${stdenv.hostPlatform.system}.devenv
   pkgs.nur.repos.charmbracelet.crush
   age
   aichat
