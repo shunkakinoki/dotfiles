ac9cdcfa0560a5e8be8492e3694e2ac7fae0b392
Shun Kakinoki <shunkakinoki@gmail.com>
2025-11-26 15:38:42 +0900

    feat: update Nix configuration to include substituters and trusted public keys for cachix

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -82,6 +82,13 @@ let
           ];
           substituters = [
             "https://cache.nixos.org"
+            "https://devenv.cachix.org"
+            "https://cachix.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+            "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
           ];
           trusted-users = [
             "root"
