69ceac0dcca4342a418840382e723b8170aeb590
Shun Kakinoki <shunkakinoki@gmail.com>
2025-11-28 14:18:02 +0900

    style: Format code for consistency in workspace.lua

--- home-manager/programs/neovim/lua/workspace.lua
+++ home-manager/programs/neovim/lua/workspace.lua
@@ -3,19 +3,19 @@
 local M = {}
 
 local function setup()
-  -- Ensure projects default to 2-space indents.
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
-    callback = function()
-      vim.opt_local.shiftwidth = 2
-      vim.opt_local.tabstop = 2
-    end,
-  })
+	-- Ensure projects default to 2-space indents.
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "*",
+		callback = function()
+			vim.opt_local.shiftwidth = 2
+			vim.opt_local.tabstop = 2
+		end,
+	})
 
-  vim.api.nvim_create_user_command("WorkspaceRoot", function()
-    local root = vim.fn.getcwd()
-    vim.notify("Workspace root: " .. root, vim.log.levels.INFO)
-  end, { desc = "Show active workspace root directory" })
+	vim.api.nvim_create_user_command("WorkspaceRoot", function()
+		local root = vim.fn.getcwd()
+		vim.notify("Workspace root: " .. root, vim.log.levels.INFO)
+	end, { desc = "Show active workspace root directory" })
 end
 
 setup()
