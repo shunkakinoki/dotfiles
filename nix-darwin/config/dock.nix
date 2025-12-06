7eb25bf4d1bed5e84c9129d8291b5699084007c5
Shun Kakinoki <shunkakinoki@gmail.com>
2025-12-07 08:38:19 +0900

    feat: remove Pushover app from persistent apps and Homebrew masApps

--- nix-darwin/config/dock.nix
+++ nix-darwin/config/dock.nix
@@ -40,7 +40,6 @@
       "/Applications/Beeper Desktop.app"
       "/Applications/Slack.app"
       "/Applications/Discord.app"
-      "/Applications/Pushover.app"
       "/System/Applications/Music.app"
       "/System/Applications/Podcasts.app"
       "/System/Applications/iPhone Mirroring.app"
