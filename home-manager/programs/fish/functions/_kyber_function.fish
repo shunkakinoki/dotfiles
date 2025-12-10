c2366cc2d5eb3e678b8a6e64842475faad3802d8
Shun Kakinoki <shunkakinoki@gmail.com>
2025-12-11 03:42:40 +0900

    feat(fish): update _kyber_function to use Tailscale for SSH access

--- home-manager/programs/fish/functions/_kyber_function.fish
+++ home-manager/programs/fish/functions/_kyber_function.fish
@@ -1,9 +1,3 @@
-function _kyber_function --description "SSH to Kyber server"
-  set -l password (security find-generic-password -s "ssh ubuntu@91.242.214.231" -w 2>/dev/null)
-  if test -n "$password"
-    sshpass -p $password ssh ubuntu@$KYBER_IP_ADDR
-  else
-    echo "Password not found in Keychain. Run: security add-generic-password -s 'ssh ubuntu@91.242.214.231' -a ubuntu -w"
-    ssh ubuntu@$KYBER_IP_ADDR
-  end
+function _kyber_function --description "SSH to Kyber server via Tailscale"
+  tailscale ssh ubuntu@kyber
 end
