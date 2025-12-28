d62776331786b0151731f54b537e67d18b4188c2
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2025-12-28 18:28:28 +0900

    fix(kybers_function): remove unnecessary -t flag from tailscale ssh command (#466)
    * fix(kybers_function): remove unnecessary -t flag from tailscale ssh command
    
    * fix(kybers_function): update SSH command to use dynamic Tailscale IP and attach to zellij main session

--- home-manager/programs/fish/functions/_kybers_function.fish
+++ home-manager/programs/fish/functions/_kybers_function.fish
@@ -1,3 +1,3 @@
 function _kybers_function --description "SSH to Kyber server via Tailscale with zellij"
-  tailscale ssh -t ubuntu@kyber zellij attach -c
+  ssh -t ubuntu@(tailscale ip -4 kyber) "zellij attach main -c"
 end
