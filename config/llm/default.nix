1558cba6c0dbfe2a7dffab78bfe2771c8593d554
Shun Kakinoki <shunkakinoki@gmail.com>
2026-01-18 00:32:30 +0900

    feat(clawdbot): add support for extra OpenAI models on Darwin and Linux

--- config/llm/default.nix
+++ config/llm/default.nix
@@ -1,6 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+  home.file."Library/Application Support/io.datasette.llm/extra-openai-models.yaml" = {
+    enable = pkgs.stdenv.isDarwin;
+    source = config.lib.file.mkOutOfStoreSymlink ./extra-openai-models.yaml;
+    force = true;
+  };
+
   home.file.".config/io.datasette.llm/extra-openai-models.yaml" = {
+    enable = pkgs.stdenv.isLinux;
     source = config.lib.file.mkOutOfStoreSymlink ./extra-openai-models.yaml;
     force = true;
   };
