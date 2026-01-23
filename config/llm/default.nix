7679e26129acdd56c5946d6c87f62a59be1cf463
Shun Kakinoki <shunkakinoki@gmail.com>
2026-01-24 07:29:54 +0900

    fix: add default_model.txt for glm-4.7 and update configurations for macOS and Linux

--- config/llm/default.nix
+++ config/llm/default.nix
@@ -6,9 +6,21 @@
     force = true;
   };
 
+  home.file."Library/Application Support/io.datasette.llm/default_model.txt" = {
+    enable = pkgs.stdenv.isDarwin;
+    source = config.lib.file.mkOutOfStoreSymlink ./default_model.txt;
+    force = true;
+  };
+
   home.file.".config/io.datasette.llm/extra-openai-models.yaml" = {
     enable = pkgs.stdenv.isLinux;
     source = config.lib.file.mkOutOfStoreSymlink ./extra-openai-models.yaml;
+    force = true;
+  };
+
+  home.file.".config/io.datasette.llm/default_model.txt" = {
+    enable = pkgs.stdenv.isLinux;
+    source = config.lib.file.mkOutOfStoreSymlink ./default_model.txt;
     force = true;
   };
 }
