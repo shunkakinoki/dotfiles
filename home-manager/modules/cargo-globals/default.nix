3586a8d1547a3475fa2f825c08a52d2b7028b603
Shun Kakinoki <shunkakinoki@gmail.com>
2026-01-23 23:31:32 +0000

    fix: conditionally apply libiconv paths only on Darwin platforms

--- home-manager/modules/cargo-globals/default.nix
+++ home-manager/modules/cargo-globals/default.nix
@@ -1,17 +1,25 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let
+  isDarwin = pkgs.stdenv.isDarwin;
+  libiconvPkgConfigPath = lib.optionalString isDarwin ":${pkgs.libiconv.dev}/lib/pkgconfig";
+  libiconvLibraryPath = lib.optionalString isDarwin "${pkgs.libiconv.out}/lib";
+  libiconvCPath = lib.optionalString isDarwin "${pkgs.libiconv.dev}/include";
+  libiconvLdFlags = lib.optionalString isDarwin "-L${pkgs.libiconv.out}/lib ";
+  libiconvCppFlags = lib.optionalString isDarwin "-I${pkgs.libiconv.dev}/include ";
+in
 {
   # Install cargo global packages from Cargo.toml using home-manager activation
   home.activation.installCargoGlobals = config.lib.dag.entryAfter [ "writeBoundary" ] ''
     export PATH=${pkgs.rustup}/bin:${pkgs.cargo}/bin:${pkgs.dasel}/bin:${pkgs.jq}/bin:${pkgs.gcc}/bin:${pkgs.pkg-config}/bin:$PATH
     export CARGO_HOME="$HOME/.cargo"
-    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.libiconv.dev}/lib/pkgconfig''${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
+    export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig${libiconvPkgConfigPath}''${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}"
     export OPENSSL_DIR="${pkgs.openssl.dev}"
     export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
     export OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include"
-    export LIBRARY_PATH="${pkgs.libiconv.out}/lib''${LIBRARY_PATH:+:$LIBRARY_PATH}"
-    export CPATH="${pkgs.libiconv.dev}/include''${CPATH:+:$CPATH}"
-    export LDFLAGS="-L${pkgs.libiconv.out}/lib ''${LDFLAGS:-}"
-    export CPPFLAGS="-I${pkgs.libiconv.dev}/include ''${CPPFLAGS:-}"
+    ${lib.optionalString isDarwin ''export LIBRARY_PATH="${libiconvLibraryPath}''${LIBRARY_PATH:+:$LIBRARY_PATH}"''}
+    ${lib.optionalString isDarwin ''export CPATH="${libiconvCPath}''${CPATH:+:$CPATH}"''}
+    ${lib.optionalString isDarwin ''export LDFLAGS="${libiconvLdFlags}''${LDFLAGS:-}"''}
+    ${lib.optionalString isDarwin ''export CPPFLAGS="${libiconvCppFlags}''${CPPFLAGS:-}"''}
     $DRY_RUN_CMD ${pkgs.bash}/bin/bash ${./install-cargo-globals.sh}
   '';
 
