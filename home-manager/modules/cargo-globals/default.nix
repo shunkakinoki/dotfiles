010bf2e2aced9be02a0f87e58f4695841399400e
Shun Kakinoki <shunkakinoki@gmail.com>
2026-01-24 01:38:55 +0000

    chore: update

--- home-manager/modules/cargo-globals/default.nix
+++ home-manager/modules/cargo-globals/default.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   isDarwin = pkgs.stdenv.isDarwin;
   libiconvPkgConfigPath = lib.optionalString isDarwin ":${pkgs.libiconv.dev}/lib/pkgconfig";
