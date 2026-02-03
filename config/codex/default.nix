bc69aff298a659f1ec2cf86b996bb1991d300228
Shun Kakinoki <shunkakinoki@gmail.com>
2026-02-03 13:45:55 +0900

    feat: replace home.file symlink with activation script for codex config

--- config/codex/default.nix
+++ config/codex/default.nix
@@ -1,7 +1,9 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
-  home.file.".codex/config.toml" = {
-    source = ./config.toml;
-    force = true;
-  };
+  # Use activation script instead of home.file symlink
+  # Codex CLI uses atomic writes that break symlinks, so we force-copy on each switch
+  home.activation.codexConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    $DRY_RUN_CMD cp -f ${./config.toml} ~/.codex/config.toml
+    $DRY_RUN_CMD chmod 600 ~/.codex/config.toml
+  '';
 }
