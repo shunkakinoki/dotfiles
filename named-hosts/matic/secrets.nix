210b92e6ddd38235a726a628fff4bea81d2fe5c5
Shun Kakinoki <shunkakinoki@gmail.com>
2026-02-07 00:42:21 +0900

    feat(matic): update secrets.nix to reflect synchronization from galactica

--- named-hosts/matic/secrets.nix
+++ named-hosts/matic/secrets.nix
@@ -1,27 +1,18 @@
-# Matic secrets - managed by agenix
-# To add a new secret:
-# 1. Add the secret definition here
-# 2. Run: make encrypt-key-matic KEY_FILE=/path/to/secret
+# Matic secrets - synced from galactica via agenix
 let
-  # Galactica's SSH public key
   galactica = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEKze2jlpV7SyTKA2ezqbumpCiDn+5Sj4z5SxrqfzesX shunkakinoki@gmail.com";
-  # Matic's SSH public key
   matic = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIQqWYSDaaoazVNOrAimCpUxNgaLe9Von237zIoox3E5 skakinoki@matic";
-  # All machines that can decrypt shared secrets
   allMachines = [
     galactica
     matic
   ];
 in
 {
-  # Shared SSH key for GitHub authentication (synced from galactica)
-  # This is the id_github key from galactica, which is the GitHub-authorized key
   "keys/id_github.age" = {
     file = ../galactica/keys/id_ed25519.age;
     publicKeys = allMachines;
   };
 
-  # GPG key for commit signing (synced from galactica)
   "keys/gpg.age" = {
     file = ../galactica/keys/gpg.age;
     publicKeys = allMachines;
