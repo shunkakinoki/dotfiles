f4c973f9056e65d1a0c97b1c2f307db80716a5c9
Shun Kakinoki <shunkakinoki@gmail.com>
2026-02-07 04:21:04 +0900

    feat(hyprland): add hyprpaper wallpaper with Catppuccin Mocha

--- config/hyprland/default.nix
+++ config/hyprland/default.nix
@@ -6,6 +6,7 @@
 }:
 let
   hyprexpoPlugin = pkgs.hyprlandPlugins.hyprexpo;
+  wallpaper = pkgs.nixos-artwork.wallpapers.nineish-catppuccin-mocha-alt;
 in
 {
   wayland.windowManager.hyprland = {
@@ -24,6 +25,15 @@ in
   };
   xdg.configFile."hypr/hyprlock.conf" = {
     source = ./hyprlock.conf;
+    force = true;
+  };
+  xdg.configFile."hypr/hyprpaper.conf" = {
+    text = ''
+      splash = false
+      ipc = on
+      preload = ${wallpaper}/share/backgrounds/nixos/nix-wallpaper-nineish-catppuccin-mocha-alt.png
+      wallpaper = ,${wallpaper}/share/backgrounds/nixos/nix-wallpaper-nineish-catppuccin-mocha-alt.png
+    '';
     force = true;
   };
 }
