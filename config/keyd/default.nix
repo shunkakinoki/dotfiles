781c776b7519b2eb909afe65878731b913d3fd86
Shun Kakinoki <shunkakinoki@gmail.com>
2026-02-07 02:29:14 +0900

    feat(keyd): add leftmeta key mapping and update restart triggers in default.nix

--- config/keyd/default.nix
+++ config/keyd/default.nix
@@ -9,5 +9,10 @@
     AmbientCapabilities = [ "CAP_SETGID" ];
   };
 
+  # Restart keyd when config changes (on make switch)
+  systemd.services.keyd.restartTriggers = [
+    (builtins.hashFile "sha256" ./default.conf)
+  ];
+
   environment.etc."keyd/default.conf".source = ./default.conf;
 }
