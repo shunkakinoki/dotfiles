ea4bcb59f9678ed1b4d89f13110c5830e1e9d949
Shun Kakinoki <shunkakinoki@gmail.com>
2026-02-09 16:20:39 +0900

    new branch

--- named-hosts/matic/default.nix
+++ named-hosts/matic/default.nix
@@ -61,8 +61,8 @@ inputs.nixpkgs.lib.nixosSystem {
         programs.fish.enable = true;
 
         # User configuration
-        users.groups.uinput.members = [ username ];
-        users.group.input.members = [ username ];
+        users.users.groups.uinput.members = [ username ];
+        users.users.group.input.members = [ username ];
         users.users.${username} = {
           isNormalUser = true;
           extraGroups = [
