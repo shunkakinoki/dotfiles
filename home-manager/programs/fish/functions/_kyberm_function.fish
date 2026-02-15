c87948fec4383d88c9caa35c95019bfab6537419
Shun Kakinoki <shunkakinoki@gmail.com>
2026-02-15 20:43:47 +0000

    feat(fish): switch kyberd/kyberm from zellij to tmux

--- home-manager/programs/fish/functions/_kyberm_function.fish
+++ home-manager/programs/fish/functions/_kyberm_function.fish
@@ -1,3 +1,3 @@
-function _kyberm_function --description "SSH to Kyber with zellij mobile session"
-  ssh -t ubuntu@(tailscale ip -4 kyber) "zellij attach mobile -c"
+function _kyberm_function --description "SSH to Kyber with tmux mobile session"
+  ssh -t ubuntu@(tailscale ip -4 kyber) "tmux new-session -A -s mobile"
 end
