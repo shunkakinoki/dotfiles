74bb9c0e0e4c9fa879487fc081618cf80f93c7eb
Shun Kakinoki <shunkakinoki@gmail.com>
2026-04-01 15:45:31 +0900

    fix(nix): gate all Library/Application Support files on isDarwin
    - iterm2: add missing isDarwin guard (would create macOS path on Linux)
    - cass: normalize from lib.mkIf to enable = isDarwin for consistency with llm/iterm2

--- config/iterm2/default.nix
+++ config/iterm2/default.nix
@@ -1,6 +1,7 @@
-{ config, ... }:
+{ pkgs, ... }:
 {
   home.file."Library/Application Support/iTerm2/DynamicProfiles/dotfiles.json" = {
+    enable = pkgs.stdenv.isDarwin;
     source = ./profile.json;
     force = true;
   };
--- home-manager/programs/cass/default.nix
+++ home-manager/programs/cass/default.nix
@@ -1,7 +1,8 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 {
   xdg.configFile."cass/sources.toml".source = ./sources.toml;
-  home.file."Library/Application Support/cass/sources.toml" = lib.mkIf pkgs.stdenv.isDarwin {
+  home.file."Library/Application Support/cass/sources.toml" = {
+    enable = pkgs.stdenv.isDarwin;
     source = ./sources.toml;
   };
 }
