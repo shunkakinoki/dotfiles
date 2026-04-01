2a7539463eda171be6ffdd86419a390141cc78b7
Shun Kakinoki <shunkakinoki@gmail.com>
2026-04-01 18:38:45 +0900

    feat: add resume flag to template fish files for model substitution

--- home-manager/programs/fish/functions/_ocxel_function.tpl.fish
+++ home-manager/programs/fish/functions/_ocxel_function.tpl.fish
@@ -2,7 +2,13 @@ function _ocxel_function --description "Run OpenCode with a free-form prompt usi
   # Run OpenCode with a free-form prompt (spaces allowed) using the local Qwen model
   # Usage: ocxel [<prompt words...>]
 
-  if test (count $argv) -eq 0
+  if test (count $argv) -gt 0; and contains -- "$argv[1]" --resume -r --continue -c
+    if test (count $argv) -gt 1
+      opencode -m 'lmstudio/__QWEN_LOCAL__' --session "$argv[2]"
+    else
+      opencode -m 'lmstudio/__QWEN_LOCAL__' --continue
+    end
+  else if test (count $argv) -eq 0
     opencode -m 'lmstudio/__QWEN_LOCAL__'
   else
     set -l prompt (string join " " -- $argv)
--- home-manager/programs/fish/functions/_pixel_function.tpl.fish
+++ home-manager/programs/fish/functions/_pixel_function.tpl.fish
@@ -2,7 +2,9 @@ function _pixel_function --description "Run Pi with a free-form prompt using the
   # Run Pi with a free-form prompt (spaces allowed) using the local Qwen model
   # Usage: pixel [<prompt words...>]
 
-  if test (count $argv) -eq 0
+  if test (count $argv) -gt 0; and contains -- "$argv[1]" --resume -r --continue -c
+    pi --model 'lmstudio/__QWEN_LOCAL__' --resume $argv[2]
+  else if test (count $argv) -eq 0
     pi --model 'lmstudio/__QWEN_LOCAL__'
   else
     set -l prompt (string join " " -- $argv)
