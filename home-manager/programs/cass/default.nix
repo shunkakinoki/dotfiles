ede11dcc0f4c44dd6356172d31703de32c04a289
Shun Kakinoki <shunkakinoki@gmail.com>
2026-04-01 15:33:04 +0900

    fix(cass): gate macOS config path on isDarwin
    The Library/Application Support path only exists on macOS.

--- home-manager/programs/cass/default.nix
+++ home-manager/programs/cass/default.nix
@@ -1,5 +1,7 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   xdg.configFile."cass/sources.toml".source = ./sources.toml;
-  home.file."Library/Application Support/cass/sources.toml".source = ./sources.toml;
+  home.file."Library/Application Support/cass/sources.toml" = lib.mkIf pkgs.stdenv.isDarwin {
+    source = ./sources.toml;
+  };
 }
