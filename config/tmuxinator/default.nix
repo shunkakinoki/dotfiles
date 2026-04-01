dea94f55b06c9c757d1f01359f3aa62619c3d03c
Shun Kakinoki <shunkakinoki@gmail.com>
2026-04-01 05:21:48 +0000

    fix: add force=true to tmuxinator configs to prevent clobber errors

--- config/tmuxinator/default.nix
+++ config/tmuxinator/default.nix
@@ -1,6 +1,18 @@
-_: {
-  xdg.configFile."tmuxinator/primary.yml".source = ./primary.yml;
-  xdg.configFile."tmuxinator/mobile.yml".source = ./mobile.yml;
-  xdg.configFile."tmuxinator/work.yml".source = ./work.yml;
-  xdg.configFile."tmuxinator/desktop.yml".source = ./desktop.yml;
+{
+  xdg.configFile."tmuxinator/primary.yml" = {
+    source = ./primary.yml;
+    force = true;
+  };
+  xdg.configFile."tmuxinator/mobile.yml" = {
+    source = ./mobile.yml;
+    force = true;
+  };
+  xdg.configFile."tmuxinator/work.yml" = {
+    source = ./work.yml;
+    force = true;
+  };
+  xdg.configFile."tmuxinator/desktop.yml" = {
+    source = ./desktop.yml;
+    force = true;
+  };
 }
