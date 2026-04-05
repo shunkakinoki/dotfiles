90c2d8818d356455c6feaff1e2d651775c9754aa
Shun Kakinoki <shunkakinoki@gmail.com>
2026-04-05 16:57:03 +0800

    feat: prefer 5GHz wifi band on matic

--- named-hosts/matic/default.nix
+++ named-hosts/matic/default.nix
@@ -73,6 +73,7 @@ inputs.nixpkgs.lib.nixosSystem {
         networking.hostName = "matic";
         networking.networkmanager.enable = true;
         networking.networkmanager.wifi.powersave = true;
+        networking.networkmanager.wifi.band = "a";
 
         # Enable fish shell
         programs.fish.enable = true;
