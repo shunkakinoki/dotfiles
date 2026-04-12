2504d732891a55b2967e53a9d548c5b0a97e4fef
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2026-04-12 22:51:25 +0800

    fix(obsidian): pass --vault flag to open wiki vault on headless launch (#1457)

--- home-manager/services/obsidian/obsidian-headless.sh
+++ home-manager/services/obsidian/obsidian-headless.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-exec @xvfbRun@/bin/xvfb-run -a @obsidian@/bin/obsidian --no-sandbox --disable-gpu "$@"
+exec @xvfbRun@/bin/xvfb-run -a @obsidian@/bin/obsidian --no-sandbox --disable-gpu --vault @homeDir@/ghq/github.com/shunkakinoki/wiki "$@"
