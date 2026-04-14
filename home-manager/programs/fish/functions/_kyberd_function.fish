f0500e75681fc28dfeb1e1aeb1b0d4d7d13339b3
Shun Kakinoki <shunkakinoki@gmail.com>
2026-04-14 23:48:12 +0800

    fix: specify ed25519 identity for kyberd ssh

--- home-manager/programs/fish/functions/_kyberd_function.fish
+++ home-manager/programs/fish/functions/_kyberd_function.fish
@@ -1,3 +1,3 @@
 function _kyberd_function --description "SSH to Kyber with zellij desktop session"
-  ssh -t ubuntu@(tailscale ip -4 kyber) "zellij attach desktop -c"
+  ssh -t -i ~/.ssh/id_ed25519 ubuntu@(tailscale ip -4 kyber) "zellij attach desktop -c"
 end
