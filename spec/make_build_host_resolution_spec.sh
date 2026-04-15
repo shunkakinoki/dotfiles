d588d75cf2796262e6016074dd99c6e08aa3dabd
Shun Kakinoki <shunkakinoki@gmail.com>
2026-04-15 21:42:45 +0800

    chore: update

--- spec/make_build_host_resolution_spec.sh
+++ spec/make_build_host_resolution_spec.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2329
+# shellcheck disable=SC2016,SC2329
 
 Describe 'Makefile nix-build host resolution'
 
@@ -15,14 +15,14 @@ Before 'setup'
 After 'cleanup'
 
 It 'uses an explicit named NixOS host on Darwin builds'
-When run bash -c "make build HOST=matic OS=Darwin ARCH=arm64 DETECTED_HOST=galactica NIX_EXEC=nix NIX_ENV=ok NIX_FLAGS= NIX_USER_TRUSTED=yes 2>/dev/null; cat \"\$MOCK_LOG\""
+When run bash -c 'make build HOST=matic OS=Darwin ARCH=arm64 DETECTED_HOST=galactica NIX_EXEC=nix NIX_ENV=ok NIX_FLAGS= NIX_USER_TRUSTED=yes 2>/dev/null; cat "$MOCK_LOG"'
 The status should be success
 The output should include '.#nixosConfigurations.matic.config.system.build.toplevel'
 The output should not include '.#darwinConfigurations.galactica.system'
 End
 
 It 'falls back to the detected Darwin host when HOST is unset'
-When run bash -c "make build OS=Darwin ARCH=arm64 DETECTED_HOST=galactica NIX_EXEC=nix NIX_ENV=ok NIX_FLAGS= NIX_USER_TRUSTED=yes 2>/dev/null; cat \"\$MOCK_LOG\""
+When run bash -c 'make build OS=Darwin ARCH=arm64 DETECTED_HOST=galactica NIX_EXEC=nix NIX_ENV=ok NIX_FLAGS= NIX_USER_TRUSTED=yes 2>/dev/null; cat "$MOCK_LOG"'
 The status should be success
 The output should include '.#darwinConfigurations.galactica.system'
 The output should not include '.#nixosConfigurations.matic.config.system.build.toplevel'
