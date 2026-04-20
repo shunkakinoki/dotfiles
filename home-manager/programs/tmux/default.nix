6388e28187cdc34095d91be14caf2a8c710f14ac
Shun Kakinoki <shunkakinoki@gmail.com>
2026-04-20 22:42:17 +0800

    fix: tmux resurrect/continuum restore on NixOS
    - Move plugin options to per-plugin extraConfig in default.nix so they
      are set before each plugin's run-shell
    - Move status-right to continuum extraConfig so continuum can prepend
      its auto-save trigger (#(.../continuum_save.sh))
    - Fix _two_function to create tmux server before resolving resurrect
      restore script path (was always empty after reboot)
    - Switch grep -E to grep -P for tab matching in post-save hook

--- home-manager/programs/tmux/default.nix
+++ home-manager/programs/tmux/default.nix
@@ -9,14 +9,38 @@
     enable = true;
     extraConfig = builtins.readFile ./tmux.conf;
     plugins = with pkgs.tmuxPlugins; [
-      continuum
-      extrakto
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '3'
+          set -g status-right "#[fg=colour250]#{?#{pane_current_command},#{pane_current_command},} #[fg=colour28]| #[fg=colour255]%Y/%m/%d %H:%M:%S #{continuum_status}"
+          set -g status-right-length 60
+        '';
+      }
+      {
+        plugin = extrakto;
+        extraConfig = "set -g @extrakto_key 'tab'";
+      }
       open
-      resurrect
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-processes 'btop fish git'
+          set -g @resurrect-hook-post-save-all 'd=~/.tmux/resurrect && f="$d/$(readlink "$d/last")" && grep -P "^(pane|window)\twork\t" "$f" > "$f.tmp" && printf "state\twork\n" >> "$f.tmp" && mv "$f.tmp" "$f"'
+        '';
+      }
       sensible
       tmux-fzf
-      tmux-sessionx
-      tmux-thumbs
+      {
+        plugin = tmux-sessionx;
+        extraConfig = "set -g @sessionx-bind 'o'";
+      }
+      {
+        plugin = tmux-thumbs;
+        extraConfig = "set -g @thumbs-key Space";
+      }
       yank
     ];
   };
