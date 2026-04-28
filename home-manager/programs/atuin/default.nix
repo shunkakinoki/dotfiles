af08a09c70311ce03b1cee6269bb048fb3f76349
Shun Kakinoki <shunkakinoki@gmail.com>
2026-04-28 17:26:09 +0900

    fix(atuin): record failed commands in history

--- home-manager/programs/atuin/default.nix
+++ home-manager/programs/atuin/default.nix
@@ -10,6 +10,9 @@
       sync_address = "https://api.atuin.sh";
       search_mode = "fuzzy";
       filter_mode = "global";
+      history = {
+        exit_code = "all";
+      };
       sync = {
         records = true;
       };
