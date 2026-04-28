85ea1d4c41f17959283f762e285f94dcc6cdffa3
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2026-04-28 17:32:23 +0900

    Revert "fix(atuin): record failed commands in history (#1606)"
    This reverts commit 71c437329fa1ed0a7fd1ba1cc7e65b15bf91effc.

--- home-manager/programs/atuin/default.nix
+++ home-manager/programs/atuin/default.nix
@@ -10,9 +10,6 @@
       sync_address = "https://api.atuin.sh";
       search_mode = "fuzzy";
       filter_mode = "global";
-      history = {
-        exit_code = "all";
-      };
       sync = {
         records = true;
       };
