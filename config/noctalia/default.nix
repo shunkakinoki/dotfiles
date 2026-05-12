67e61ae306ee3e2a96d2262cc27e2c0d1bbc54f8
Shun Kakinoki <shunkakinoki@gmail.com>
2026-05-12 21:50:15 +0800

    fix(noctalia): add coreutils to ac-idle-inhibit PATH for sleep command

--- config/noctalia/default.nix
+++ config/noctalia/default.nix
@@ -26,7 +26,7 @@ in
     Service = {
       Type = "simple";
       PassEnvironment = "WAYLAND_DISPLAY";
-      Environment = "PATH=${pkgs.wlinhibit}/bin";
+      Environment = "PATH=${pkgs.wlinhibit}/bin:${pkgs.coreutils}/bin";
       ExecStart = "${pkgs.bash}/bin/bash ${./ac-idle-inhibit.sh}";
       Restart = "on-failure";
     };
