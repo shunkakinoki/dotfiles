d9e5deb774fc7d1fd762df1f431946ee4fc1e4e7
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2026-06-02 19:20:44 +0900

    fix(spec): align ssh config tests with settings format (#1882)

--- spec/ssh_config_spec.sh
+++ spec/ssh_config_spec.sh
@@ -6,14 +6,14 @@ SSH_CONFIG_NIX="$PWD/home-manager/programs/ssh/default.nix"
 
 Describe 'kyber host'
 It 'uses the Tailscale DNS name instead of a stale tailnet IP'
-When run bash -c "grep 'hostname =' '$SSH_CONFIG_NIX' | grep kyber"
+When run bash -c "grep 'HostName =' '$SSH_CONFIG_NIX' | grep kyber"
 The output should include 'kyber.tail950b36.ts.net'
 The output should not include '100.74.174.97'
 End
 
 It 'pins the Codex-compatible SSH identity'
 When run cat "$SSH_CONFIG_NIX"
-The output should include 'identityFile = "~/.ssh/id_rsa"'
+The output should include 'IdentityFile = "~/.ssh/id_rsa"'
 The output should include 'IdentitiesOnly = "yes"'
 End
 End
