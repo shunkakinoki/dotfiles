87e3e5c72818bf3d937e49eab83bb0d7d90d775c
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2026-06-02 18:53:35 +0900

    fix(home-manager): migrate SSH config from matchBlocks to settings (#1877)

--- home-manager/programs/ssh/default.nix
+++ home-manager/programs/ssh/default.nix
@@ -7,51 +7,36 @@ _: {
     enable = true;
     enableDefaultConfig = false;
     includes = [ "~/.ssh/config.local" ];
-    matchBlocks = {
+    settings = {
       "*" = {
-        serverAliveInterval = 60;
-        identityFile = "~/.ssh/id_ed25519";
-        setEnv = {
+        ServerAliveInterval = 60;
+        IdentityFile = "~/.ssh/id_ed25519";
+        SetEnv = {
           TERM = "xterm-256color";
         };
-        sendEnv = [
-          "COLORTERM"
-        ];
-        extraOptions = {
-          IgnoreUnknown = "UseKeychain";
-          UseKeyChain = "yes";
-          # Enable post-quantum key exchange algorithms
-          # sntrup761x25519 is a hybrid post-quantum algorithm combining
-          # Streamlined NTRU Prime (sntrup761) with X25519
-          KexAlgorithms = "sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256";
-          # Prefer modern host key algorithms
-          HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-ed25519,rsa-sha2-512-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-256";
-          # Prefer modern public key algorithms
-          PubkeyAcceptedAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-ed25519,rsa-sha2-512-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-256";
-        };
+        SendEnv = [ "COLORTERM" ];
+        IgnoreUnknown = "UseKeychain";
+        UseKeyChain = "yes";
+        KexAlgorithms = "sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256";
+        HostKeyAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-ed25519,rsa-sha2-512-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-256";
+        PubkeyAcceptedAlgorithms = "ssh-ed25519-cert-v01@openssh.com,ssh-ed25519,rsa-sha2-512-cert-v01@openssh.com,rsa-sha2-512,rsa-sha2-256-cert-v01@openssh.com,rsa-sha2-256";
       };
       "localhost" = {
-        extraOptions = {
-          UserKnownHostsFile = "/dev/null";
-          StrictHostKeyChecking = "false";
-        };
+        UserKnownHostsFile = "/dev/null";
+        StrictHostKeyChecking = "false";
       };
       "kyber" = {
-        hostname = "kyber.tail950b36.ts.net";
-        user = "ubuntu";
-        identityFile = "~/.ssh/id_rsa";
-        extraOptions = {
-          IdentitiesOnly = "yes";
-        };
+        HostName = "kyber.tail950b36.ts.net";
+        User = "ubuntu";
+        IdentityFile = "~/.ssh/id_rsa";
+        IdentitiesOnly = "yes";
       };
       "github.com" = {
-        serverAliveInterval = 0;
-        identityFile = "~/.ssh/id_ed25519_github";
-        extraOptions = {
-          ControlMaster = "auto";
-          ControlPath = "~/.ssh/github.sock";
-          ControlPersist = "3m";
-        };
+        ServerAliveInterval = 0;
+        IdentityFile = "~/.ssh/id_ed25519_github";
+        ControlMaster = "auto";
+        ControlPath = "~/.ssh/github.sock";
+        ControlPersist = "3m";
       };
     };
   };
