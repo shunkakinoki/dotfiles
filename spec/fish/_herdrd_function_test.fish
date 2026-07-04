26b71fbe8d84c489f491631af494b1c9cea729aa
Shun Kakinoki <shunkakinoki@gmail.com>
2026-07-04 13:37:03 +0000

    fix: add missing _herdrd_function fish test

---
+++ spec/fish/_herdrd_function_test.fish
@@ -0,0 +1,12 @@
+set fn (status dirname)/../../home-manager/programs/fish/functions
+source $fn/_herdrd_function.fish
+
+set call_log (mktemp)
+
+function herdr; echo $argv >> $call_log; end
+
+_herdrd_function
+
+@test "passes --remote kyber" (grep -c "\-\-remote kyber" $call_log) -ge 1
+
+rm -f $call_log
