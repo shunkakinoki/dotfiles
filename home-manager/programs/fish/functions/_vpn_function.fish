b731af7a8473bfe22bfc647007974c04405fa2e7
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2026-07-10 06:17:55 +0800

    fix: use kyber hostname for vpn exit node and fix toggle detection (#2033)

--- home-manager/programs/fish/functions/_vpn_function.fish
+++ home-manager/programs/fish/functions/_vpn_function.fish
@@ -1,21 +1,21 @@
 function _vpn_function --description "Connect/disconnect Tailscale exit node through kyber"
-  set -l kyber_ip 100.74.174.97
+  set -l kyber_host kyber
 
   switch "$argv[1]"
     case on
-      sudo tailscale set --exit-node=$kyber_ip
+      sudo tailscale set --exit-node=$kyber_host
     case off
       sudo tailscale set --exit-node=
     case status
       tailscale status
     case ''
       # Toggle: if exit node is set, turn off; otherwise turn on
-      set -l current (tailscale status --json 2>/dev/null | string match -rg '"ExitNodeStatus"')
+      set -l current (tailscale status --json 2>/dev/null | jq -r '.ExitNodeStatus.ID // empty')
       if test -n "$current"
         sudo tailscale set --exit-node=
         echo "VPN disconnected"
       else
-        sudo tailscale set --exit-node=$kyber_ip
+        sudo tailscale set --exit-node=$kyber_host
         echo "VPN connected through kyber"
       end
     case '*'
