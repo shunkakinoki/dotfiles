c6561df67b53b9ab0731302516064d41c2ebeb56
Shun Kakinoki <shunkakinoki@gmail.com>
2026-07-11 02:39:08 +0800

    fix(fish): grs aborts on unmerged branches, resets cleanly to origin default

--- home-manager/programs/fish/functions/_grs_function.fish
+++ home-manager/programs/fish/functions/_grs_function.fish
@@ -1,3 +1,11 @@
-function _grs_function --description "Reset to origin default branch"
-    git fetch origin && git reset --hard origin/(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+function _grs_function --description "Reset to origin default branch (safe: aborts if unmerged)"
+    set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+    git fetch origin
+
+    if not git merge-base --is-ancestor HEAD origin/$default_branch
+        echo "grs: current branch is not merged into origin/$default_branch, aborting." >&2
+        return 1
+    end
+
+    git reset --hard origin/$default_branch
 end
--- spec/fish/_grs_function_test.fish
+++ spec/fish/_grs_function_test.fish
@@ -1,12 +1,16 @@
 set fn (status dirname)/../../home-manager/programs/fish/functions
 source $fn/_grs_function.fish
 
+# --- Test: merged branch resets to origin ---
+
 set call_log (mktemp)
 
 function git
     echo $argv >> $call_log
     if test "$argv[1]" = symbolic-ref
         echo "refs/remotes/origin/main"
+    else if test "$argv[1]" = merge-base
+        return 0
     end
 end
 
@@ -17,6 +21,34 @@ end
 _grs_function
 
 @test "fetches origin" (grep -c "fetch origin" $call_log) -ge 1
+@test "checks merge status" (grep -c "merge-base" $call_log) -ge 1
 @test "resets to origin default branch" (grep -c "reset --hard origin/main" $call_log) -ge 1
 
 rm -f $call_log
+
+# --- Test: unmerged branch aborts ---
+
+set call_log (mktemp)
+set err_log (mktemp)
+
+function git
+    echo $argv >> $call_log
+    if test "$argv[1]" = symbolic-ref
+        echo "refs/remotes/origin/main"
+    else if test "$argv[1]" = merge-base
+        return 1
+    end
+end
+
+function sed
+    echo "main"
+end
+
+_grs_function 2>$err_log
+set exit_code $status
+
+@test "unmerged: aborts with error" $exit_code -eq 1
+@test "unmerged: no reset" (grep -c "reset" $call_log) -eq 0
+@test "unmerged: prints warning" (grep -c "not merged" $err_log) -ge 1
+
+rm -f $call_log $err_log
