797139fc91ec9dd11e7548149c38ef26f42b255e
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2026-07-17 07:55:31 +0800

    feat(gh): add gh-stack extension (#2086)

--- home-manager/programs/gh/default.nix
+++ home-manager/programs/gh/default.nix
@@ -2,7 +2,7 @@
 {
   programs.gh = {
     enable = true;
-    extensions = with pkgs; [ gh-markdown-preview ];
+    extensions = with pkgs; [ gh-markdown-preview gh-stack ];
     settings = {
       editor = "nvim";
       git_protocol = "https";
