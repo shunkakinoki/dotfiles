a8f2cdf55a3297168984ec6b291611f1e400ddce
Shun Kakinoki <shunkakinoki@gmail.com>
2026-07-17 12:38:49 +0800

    style(gh): format extensions list
    treefmt expands multi-element lists one entry per line. Unformatted
    since the gh-stack extension was added in 797139fc.

--- home-manager/programs/gh/default.nix
+++ home-manager/programs/gh/default.nix
@@ -2,7 +2,10 @@
 {
   programs.gh = {
     enable = true;
-    extensions = with pkgs; [ gh-markdown-preview gh-stack ];
+    extensions = with pkgs; [
+      gh-markdown-preview
+      gh-stack
+    ];
     settings = {
       editor = "nvim";
       git_protocol = "https";
