658ccd79d7a85d815b99e6cbd6af6be2603a98a9
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2026-07-29 15:16:24 +0800

    fix(kyber): rotate age identity (#2175)

--- named-hosts/pubkeys.nix
+++ named-hosts/pubkeys.nix
@@ -3,6 +3,6 @@
 # activation script that needs to authorize cross-host access.
 {
   galactica = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEKze2jlpV7SyTKA2ezqbumpCiDn+5Sj4z5SxrqfzesX shunkakinoki@gmail.com";
-  kyber = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO0IZtP3KSzY6GVSZ+R+VQYYfu3sEOVaQGDblQxAtwNM ubuntu@kyber";
+  kyber = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICbJMYUWpV2mQhfRnKWLaxRATS7+yvE7u2IYXic6/rIZ ubuntu@kyber";
   matic = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOsMGpqklcznrSAH/TiGvcJoHEF4hyf5yiRz9MDjVVUj skakinoki@matic";
 }
