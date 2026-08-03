b31098638f61f2e50f64235f4db17f2378c22276
Shun Kakinoki <shunkakinoki@gmail.com>
2026-08-03 10:43:13 +0800

    fix(nix): pass home manager args to nixos users

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -157,15 +157,17 @@ let
     inputs.home-manager.nixosModules.home-manager
     {
       home-manager.backupFileExtension = "hm-backup";
-      home-manager.extraSpecialArgs = { inherit inputs isRunner; };
+      home-manager.extraSpecialArgs = {
+        inherit
+          inputs
+          isRunner
+          pkgs
+          username
+          ;
+      };
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
-      home-manager.users."${username}" = import ../../home-manager {
-        inherit inputs username isRunner;
-        inherit (inputs.nixpkgs) lib;
-        inherit pkgs;
-        config = { };
-      };
+      home-manager.users."${username}" = ../../home-manager;
     }
   ];
 in
