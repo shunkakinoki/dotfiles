da62f1e975a41b70b759a307d09fadce75cc52ad
Shun Kakinoki <shunkakinoki@gmail.com>
2026-08-05 09:11:12 +0800

    fix(matic): restore Tailscale MagicDNS resolution
    Let systemd-resolved own matic's resolver so Tailscale can register split DNS routes while retaining the existing public upstream nameservers.

--- spec/matic_tailscale_spec.sh
+++ spec/matic_tailscale_spec.sh
@@ -24,6 +24,27 @@ When run bash -c "grep -F '\"--accept-dns=true\"' '$CONFIG'"
 The output should include '--accept-dns=true'
 End
 
+It 'uses systemd-resolved for Tailscale MagicDNS'
+When run bash -c "grep -F 'services.resolved.enable = true;' '$CONFIG'"
+The output should include 'services.resolved.enable = true'
+End
+
+It 'routes NetworkManager DNS through systemd-resolved'
+When run bash -c "grep -A2 -F 'networking.networkmanager = {' '$CONFIG'"
+The output should include 'systemd-resolved'
+End
+
+It 'preserves public upstream resolvers'
+When run bash -c "grep -A5 -F 'networking.nameservers = [' '$CONFIG'"
+The output should include '8.8.8.8'
+The output should include '1.1.1.1'
+End
+
+It 'releases the shared static resolv.conf override'
+When run bash -c "grep -F 'environment.etc.\"resolv.conf\".text = lib.mkForce null;' '$CONFIG'"
+The output should include 'lib.mkForce null'
+End
+
 It 'trusts the tailscale interface in the firewall'
 When run bash -c "grep -F 'trustedInterfaces = [ \"tailscale0\" ];' '$CONFIG'"
 The output should include 'tailscale0'
