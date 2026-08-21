45a6c95faff43d5696b47617f1c7a9f64c59a7a3
shunkakinoki <39187513+shunkakinoki@users.noreply.github.com>
2026-08-21 12:27:26 +0000

    chore: upgrade

--- overlays/default.nix
+++ overlays/default.nix
@@ -106,20 +106,20 @@
   (_: prev: {
     moshi-hook = prev.stdenv.mkDerivation rec {
       pname = "moshi-hook";
-      version = "0.2.87";
+      version = "0.3.0";
       src = prev.fetchurl {
         url = "https://cdn.getmoshi.app/hook/v${version}/moshi-hook_${
           if prev.stdenv.isDarwin then "Darwin" else "Linux"
         }_${if prev.stdenv.hostPlatform.isAarch64 then "arm64" else "x86_64"}.tar.gz";
         sha256 =
           if prev.stdenv.isLinux && prev.stdenv.hostPlatform.isx86_64 then
-            "6770dab4f61f1c1a8e8263a757bb4c3a4f8fe92694df1a8b5cf21e5f127fbe91"
+            "9ca3ff58df82b9092191e51855aed3435a4f6d5f32062d1f0ee5a66893046990"
           else if prev.stdenv.isLinux && prev.stdenv.hostPlatform.isAarch64 then
-            "7b9ae9b2bd7b234181a68d6c61cfd8655e2b951bd3417703f074c3a15c058b81"
+            "857f283d8e27b6aa47f07120a53497af6ea59025b8c819a29fc023fad7d02180"
           else if prev.stdenv.isDarwin && prev.stdenv.hostPlatform.isAarch64 then
-            "f7b9ca02320ef263aa7cdd32dee4015f193a0294ecbca884cfdeb9cd3fc11870"
+            "78dd7164b37abb94ddbccc147f4c5af12d47288af5c8f4c3841acb7b58c2d25e"
           else
-            "f8d95fbf8910418139c3ab055ae6bb004a3a6ab8332632ff83eaa846f3e67c38";
+            "ab9dfc77bf1525b1f9366e06c74555738d3e5bb08a85d4657df85860815bf10f";
       };
       sourceRoot = ".";
       dontConfigure = true;
