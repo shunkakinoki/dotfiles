10b5ffdb5fdf579a1950fded06fc3124af30f679
Shun Kakinoki <shunkakinoki@gmail.com>
2026-09-13 19:41:07 +0800

    test: update kamino shortcut tailscale fixture

--- tests/kamino-shortcuts.fish
+++ tests/kamino-shortcuts.fish
@@ -9,6 +9,11 @@ function ssh
     return $command_status
 end
 
+function tailscale
+    set -g called tailscale $argv
+    return $command_status
+end
+
 function herdr
     set -g called herdr $argv
     return $command_status
@@ -31,11 +36,11 @@ set -g command_status 0
 for host in $argv
     set -l shortcut _{$host}_function
     $shortcut 'printf hello' 'two words'
-    expect_call ssh $host 'printf hello' 'two words'
+    expect_call tailscale ssh $host 'printf hello' 'two words'
 
     set shortcut _{$host}d_function
     $shortcut
-    expect_call ssh -t $host 'tmux new-session -A -s desktop'
+    expect_call tailscale ssh $host 'tmux new-session -A -s desktop'
 
     set shortcut _{$host}h_function
     $shortcut --help 'two words'
@@ -43,11 +48,11 @@ for host in $argv
 
     set shortcut _{$host}m_function
     $shortcut
-    expect_call ssh -t $host 'tmux new-session -A -s mobile'
+    expect_call tailscale ssh $host 'tmux new-session -A -s mobile'
 
     set shortcut _{$host}z_function
     $shortcut
-    expect_call ssh -t $host 'zellij attach -c desktop'
+    expect_call tailscale ssh $host 'zellij attach -c desktop'
 
     set -g command_status 23
     for suffix in '' d h m z
