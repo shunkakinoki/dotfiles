87d7e1d6f6a135a14a3c185bac268ca8c39f20b9
Shun Kakinoki <shunkakinoki@gmail.com>
2026-09-19 12:19:13 +0800

    fix(git): disable fsmonitor to cut FSEvents churn

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 {
   programs = {
     git = {
@@ -23,9 +23,6 @@
           precomposeunicode = true;
           ignorecase = false;
           notesRef = "refs/notes/ai";
-          # Git ships its builtin fsmonitor daemon only for macOS and Windows; on
-          # Linux hosts the setting adds seconds to every git call instead.
-          fsmonitor = pkgs.stdenv.hostPlatform.isDarwin;
           untrackedcache = true;
         };
         feature = {
