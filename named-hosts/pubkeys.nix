0000c9aaa6f79687a8cec120234bd3cca347324b
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2026-09-26 04:27:58 +0800

    fix(kamino): enroll kamino7 for GPG activation (#2934)

--- named-hosts/pubkeys.nix
+++ named-hosts/pubkeys.nix
@@ -11,4 +11,5 @@
   kamino4 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGXNZvPW1xedam57pTusOs1Pwsfbcs6OiAweb7f7TGr4 root@kamino4";
   kamino5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtqqndnmPZKn3l6bNo34GutU4eCQMDn4AC57lh15TTR root@kamino5";
   kamino6 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2F6w6n8+6PxxukjZfvWL3Fnozhq7PXJJXI1D1C4Dkm root@kamino6";
+  kamino7 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAurx8K6kgs4Z11raw3DG55MYxGuSufQaEzr8VlpOEg3 root@kamino7";
 }
